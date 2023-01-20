9a1123a4ab47c24963ec2478cf90a55489c2588d
vitoraugustohu <vitoraugustodev.g@gmail.com>
2023-01-20 18:41:38 -0300

    feat: create component folder

---
+++ src/components/Presentation/index.tsx
@@ -0,0 +1,18 @@
+import styles from './styles.module.scss'
+
+const Presentation = () => {
+  return (
+    <>
+      <section className={styles.presetantion__container}>
+        <h1>Desenvolvedor Front-End</h1>
+        <p>
+          Me chamo Vitor Augusto e atuo como Front-end Developer. Meu trabalho
+          vai muito além de código, eu crio soluções e os usuários são sempre o
+          centro delas.
+        </p>
+      </section>
+    </>
+  )
+}
+
+export default Presentation
